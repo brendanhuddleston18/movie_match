6fae95c3473ca5c9337b795243657e5a9aa95aef
Brendan Huddleston <brendanhuddleston18@gmail.com>
2024-08-27 17:21:22 -0500

    Creates DB service that initilizes Supabase instance and two functions(getWL/addMoviesToWL)

--- lib/services/database_service.dart
+++ lib/services/database_service.dart
@@ -1,11 +1,39 @@
+import 'package:movie_match/classes/movie_class.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:get/get.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:uuid/uuid.dart';
+
+DatabaseService database = Get.isRegistered<DatabaseService>()
+    ? Get.find<DatabaseService>()
+    : Get.put(DatabaseService());
 
 class DatabaseService extends GetxService {
-  
-  Future<void> init() async{
-    
+  late SupabaseClient supabase;
+  Future<void> init() async {
+    await dotenv.load(fileName: '.env');
+    String url = dotenv.env['URL']!;
+    String anonKey = dotenv.env['ANONKEY']!;
 
+    await Supabase.initialize(url: url, anonKey: anonKey);
+    supabase = Supabase.instance.client;
   }
-  
+
+  Future<List<Map>> getWatchlist() async {
+    final data = await supabase.from('watchlist').select('*');
+    return data;
+  }
+
+  Future<void> addMovieToWatchlist(Movie movie) async {
+    await supabase.from('watchlist').insert({
+      'id': movie.id,
+      'name': movie.title,
+      'image': movie.image,
+      'description': movie.description,
+      'movie_cast': movie.cast,
+      'rating': movie.rating
+    });
+  }
+
+  // TODO: Make a delete movie from watchlist function
 }
